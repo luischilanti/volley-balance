b5f9aac1dbe78e09f54b4362f04f5a8153641573
Luis Chilanti Brollo <luischilanti@hotmail.com>
2024-03-30 01:04:26 -0300

    adding configs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 
 import { router } from "./routes"
+import "./styles/index.scss"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
